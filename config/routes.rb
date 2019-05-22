4c0af3d3c061034f9822493a3591b4be3dca1ad8
TGRaydas <pgrand@miuandes.cl>
2019-05-21 21:39:25 -0400

    Lab10 BaseCode

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
 
@@ -9,5 +10,6 @@ Rails.application.routes.draw do
 
   get '/sign_in' => 'registrations#new', as: :registrations
   post '/sign_in' => 'registrations#create', as: :sign_in
+
 
 end
